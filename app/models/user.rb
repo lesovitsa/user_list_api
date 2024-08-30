e77b0c586fadbfec74268825c3dd0906a90d790e
Yana Mishkova-Kareva <yanamishkova@gmail.com>
2024-08-30 14:49:26 +0300

    Issue 2: Create necessary structures

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :phone, presence: true
+    validates :name, format: { with: /\A\w+(\s\w+)*\z/i }
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+    validates :phone, format: { with: /\A((\+\d{10,12})|(\d{10}))\z/i } # we want to be able to take both +359000000000 and 0000000000
+end
